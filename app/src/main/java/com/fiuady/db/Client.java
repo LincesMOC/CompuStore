f42d9e49f20e81f49c3ae0c11dad08aac6c2a471
maarsrawr13 <maars_rawr@hotmail.com>
2017-04-01 17:58:09 -0600

    Llené clase Client

--- app/src/main/java/com/fiuady/db/Client.java
+++ app/src/main/java/com/fiuady/db/Client.java
@@ -1,8 +1,43 @@
 package com.fiuady.db;
 
-/**
- * Created by Mariana on 01/04/2017.
- */
 
 public class Client {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phone1;
+    private String phone2;
+    private String phone3;
+    private String email;
+
+    public Client(int id, String firstName, String lastName, String address,
+                  String phone1, String phone2, String phone3, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.phone1 = phone1;
+        this.phone2 = phone2;
+        this.phone3 = phone3;
+        this.email = email;
+    }
+
+    public int getId() {return id;}
+
+    public String getFirstName() {return firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public String getAddress() {return address;}
+
+    public String getPhone1() {return phone1;}
+
+    public String getPhone2() {return phone2;}
+
+    public String getPhone3() {return phone3;}
+
+    public String getEmail() {return email;}
+
 }
